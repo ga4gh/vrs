45454ef1bb36ee867fbf7ac058e3be5f877306fe
Reece Hart <reecehart@gmail.com>
2017-05-10 17:24:40 -0700

    removed openapi code and demo, added jsonschema versions (in progress)

--- demo-python/vmcdemo/__init__.py
+++ demo-python/vmcdemo/__init__.py
@@ -1,8 +1,1 @@
-import os
-
-import pkg_resources
-
-
-spec_dir = pkg_resources.resource_filename(__name__, "_data/spec")
-spec_path = spec_dir + "/vmc.yaml"
-spec_file = os.path.basename(spec_path)
+from ._models import models
---
+++ demo-python/vmcdemo/_models.py
@@ -0,0 +1,17 @@
+"""vmcdemo models, defined at runtime from the spec
+
+"""
+
+import os
+
+import pkg_resources
+import python_jsonschema_objects as pjs
+
+
+schema_dir = pkg_resources.resource_filename(__name__, "_data/schema")
+schema_path = schema_dir + "/vmcbundle.json"
+schema_file = os.path.basename(schema_path)
+
+
+builder = pjs.ObjectBuilder(schema_path)
+models = builder.build_classes()
