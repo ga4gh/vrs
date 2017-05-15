29091f20d8ca147adc0c1dd3e3c3cd0f14b289da
Reece Hart <reecehart@gmail.com>
2017-05-15 08:43:47 -0700

    added ApoE example

--- demo/vmcdemo/__init__.py
+++ demo/vmcdemo/__init__.py
@@ -1,3 +1,5 @@
 # flake8: noqa
-from ._models import models
+
+from ._models import models, schema_path
 from .digest import computed_id, computed_identifier, digest, serialize
+from .seqrepo import get_vmc_sequence_id
