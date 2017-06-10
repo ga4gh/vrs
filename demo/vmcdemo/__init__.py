30e8a2b3ea2563e1df5418977e413c4a2f6f9008
Reece Hart <reecehart@gmail.com>
2017-06-10 10:01:09 +0200

    updated for new Identifier serialization

--- demo/vmcdemo/__init__.py
+++ demo/vmcdemo/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
 
 from ._models import models, schema_path
-from .digest import computed_id, computed_identifier, digest, serialize
+from .digest import computed_id, computed_identifier, truncated_digest, serialize
 from .seqrepo import get_vmc_sequence_id
--- demo/vmcdemo/digest.py
+++ demo/vmcdemo/digest.py
@@ -1,6 +1,6 @@
 """Provides format and digest functions for VMC objects
 
-A VMC digest is computed using a `truncated_digest` (see below) on a
+A VMC digest is computed using a "truncated digest" (see below) on a
 well-prescribed serialization of an object.
 
 To compute the digest of an object:
@@ -19,83 +19,137 @@ To serialize an object, all properties except the `id` field are
 formatted as strings and concatenated, separated by a colon.  When a
 property to be serialized is an object, 
 
+
+The computed_identifer is functionally this:
+
+  ci = computed_identifier(obj)
+
+The pseudocode for computed_identifier is:
+
+def computed_identifier(obj):
+  ser = serialize( obj )
+  digest = base64_urlsafe( sha512( ser )[:24] )
+  prefix = vmc_type_prefix[type(obj)]
+  accession = prefix + "_" + digest
+  ir = Identifier(namespace="VMC", accession=accession)
+
 """
 
-
-from bioutils.digests import truncated_digest
+import base64
+import hashlib
 
 from . import models
 
 abc = models
 
-namespace = "VMC"
-namespacec = namespace + ":"
-model_prefixes = {
+enc = "ASCII"
+
+vmc_namespace = "VMC"
+vmc_model_prefixes = {
     # GS: Sequence does not have a model
-    models.Location: "GL",
     models.Allele: "GA",
-    models.Haplotype: "GH",
     models.Genotype: "GG",
+    models.Haplotype: "GH",
+    models.Location: "GL",
 }
 
 
-
 def computed_id(o):
-    if o.id is not None and o.id.startswith(namespacec):
-        return o.id
-    ident = computed_identifier(o)
-    return serialize(ident)
-
-
-def computed_identifier(o):
-    """return the VMC computed identifier for the object
+    """return the VMC computed identifier for the object, as a CURIE
+    (string)
 
     """
 
-    if o.id is not None and o.id.startswith(namespacec):
-        return models.Identifier(o.id.split(":"))
-
-    accession = "{ns}_{d}".format(ns=model_prefixes[type(o)], d=digest(o))
-    return models.Identifier(namespace=namespace, accession=accession)
+    return "{i.namespace}:{i.accession}".format(i=computed_identifier(o))
 
 
-def digest(o):
-    s = serialize(o)
-    return truncated_digest(s.encode("UTF-8"), digest_size=24)
+def computed_identifier(o):
+    """return the VMC computed identifier for the object, as an Identifier
+
+    """
+
+    pfx = vmc_model_prefixes[type(o)]
+    dig = truncated_digest(o)
+    accession = "{pfx}_{dig}".format(pfx=pfx, dig=dig)
+    return models.Identifier(namespace=vmc_namespace, accession=accession)
+
+
+def id_to_ir(id):
+    """Convert internal id to identifier string.
+
+    For the VMC demo, we're going to assume that the computed
+    identifier is used as the id, and therefore we can just assert
+    that the id begins with the "VMC:" and then return a synthesized
+    Identifer.
+
+    If an implementation uses a different internal id, this function
+    needs to be changed.
+
+    """
+    assert id.startswith(vmc_namespace + ":"), "Must be a VMC id for this demo"
+    ns, acc = id.split(":")
+    return models.Identifier(namespace=ns, accession=acc)
 
 
 def serialize(o, namespace="VMC"):
-    def _map_id(id):
-        assert id.startswith(namespace + ":")
-        return id
+    """convert object to canonical serialized representation
 
-    # isinstance() fails here because nested classes are coerced into
-    # the `abc` namespace.  (I don't completely understand why this
-    # happens. We'll use the class "basename".
+    Serialization is the core of the VMC digest algorithm.  All of the
+    magic occurs here.
+
+    Two wrinkles:
+
+    * The digest must be based on VMC (external) identifiers. For this
+      demo, the VMC identifiers and internal ids are equivalent, but
+      that need not be true generally.  For example, an implementation
+      might choose to use uuids for ids, with VMC identifiers
+      attached.  (See id_to_ir for shortcut used in this demo.)
+
+    * Alleles need to be reliably ordered.  Unfortunately, URL-safe
+      Base 64 encodings use characters that are subject to
+      locale-dependent sort differences.  So, we sort here by the
+      ASCII-encoded form (essentially, binary sorted).
+
+    """
+
+    # isinstance() fails here because nested classes built with
+    # python_jsonschema_objects are coerced into the `abc` namespace.
+    # So, we'll use the class "basename".
     t = o.__class__.__name__
 
     if t == "Identifier":
-        return "{o.namespace}:{o.accession}".format(o=o)
+        return "<{t}:{o.namespace}:{o.accession}>".format(t=t, o=o)
 
     if t == "Interval":
         return "<{t}:{o.start}:{o.end}>".format(t=t, o=o)
 
     if t == "Location":
-        return "<{t}:{id}:{ival}>".format(t=t, id=_map_id(o.sequence_id), ival=serialize(o.interval))
+        ir = id_to_ir(o.sequence_id)
+        return "<{t}:{ir}:{ival}>".format(t=t, ir=serialize(ir), ival=serialize(o.interval))
 
     if t == "Allele":
-        return "<{t}:{id}:{o.state}>".format(t=t, id=_map_id(o.location_id), o=o)
+        ir = id_to_ir(o.location_id)
+        return "<{t}:{ir}:{o.state}>".format(t=t, ir=serialize(ir), o=o)
 
     if t == "Haplotype":
-        ids = sorted(_map_id(str(i)).encode("UTF-8") for i in o.allele_ids)
-        return "<{t}:{o.completeness}:[{ids}]>".format(t=t, o=o, ids=";".join(i.decode("UTF-8") for i in ids))
+        # sort as well-defined binary encoding to circumvent locale-dependent sorting differences
+        ids = sorted(serialize(id_to_ir(str(i))).encode(enc) for i in o.allele_ids)
+        return "<{t}:{o.completeness}:[{irss}]>".format(t=t, o=o, irss=";".join(i.decode(enc) for i in ids))
 
     if t == "Genotype":
-        ids = sorted(_map_id(str(i)).encode("UTF-8") for i in o.haplotype_ids)
-        return "<{t}:{o.completeness}:[{ids}]>".format(t=t, o=o, ids=";".join(i.decode("UTF-8") for i in ids))
+        # sort as well-defined binary encoding to circumvent locale-dependent sorting differences
+        ids = sorted(serialize(id_to_ir(str(i))).encode(enc) for i in o.haplotype_ids)
+        return "<{t}:{o.completeness}:[{irss}]>".format(t=t, o=o, irss=";".join(i.decode(enc) for i in ids))
 
     raise Exception("Unknown type: " + t)
 
 
+def truncated_digest(o):
+    """For a VMC object o, return the URL-safe, Base64 encoded, 24-byte
+    truncated SHA512 digest as unicode
 
-
+    """
+    ser = serialize(o)
+    digest = hashlib.sha512(ser.encode(enc)).digest()
+    tdigest_b64us = base64.urlsafe_b64encode(digest[:24]).decode(enc)
+    return tdigest_b64us
