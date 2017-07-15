984dc3a1aad799c4e67ef8a46b8f00472dd35741
Reece Hart <reecehart@gmail.com>
2017-07-15 07:14:00 -0700

    use namespace verbatim in seqrepo sequence lookup

--- demos/python/vmcdemo/seqrepo.py
+++ demos/python/vmcdemo/seqrepo.py
@@ -4,13 +4,7 @@ _sr = biocommons.seqrepo.SeqRepo("/usr/local/share/seqrepo/master")
 
 
 def get_vmc_sequence_id(ir):
-    # TODO: seqrepo will switch to no case squashing at somepoint :-(
-
-    # TODO: This uses the fact that we know that seqrepo uses a
-    # truncated digest (SHA512/24) for sequence identfiers.  This
-    # should be treated as a private implementation detail.  For that
-    # to happen, seqrepo needs to rename the `sh` namespace to
-    # `sha512t24` or somesuch.
-
-    r = _sr.aliases.find_aliases(namespace=ir.namespace.lower(), alias=ir.accession).fetchone()
+    r = _sr.aliases.find_aliases(namespace=ir.namespace, alias=ir.accession).fetchone()
+    if r is None:
+        raise KeyError(ir)
     return "VMC:GS_" + r["seq_id"]
