f141e9d33bc8533a4d49fcac535bcf77cb45c517
Reece Hart <reecehart@gmail.com>
2021-07-09 17:03:30 -0700

    add docstrings to appease pylint

--- schema/helpers.py
+++ schema/helpers.py
@@ -1,9 +1,18 @@
+"""miscellaneous functions for vrs-python
+
+"""
+
 import logging
 
 _logger = logging.getLogger(__name__)
 
 
 def pjs_filter(yaml_dict):
+    """filter out schema elements that are not supported by python
+    jsonschema objects (yet)
+
+    """
+
     for message_name, message_definition in yaml_dict['definitions'].items():
         if 'anyOf' in message_definition:
             _logger.warning(f'Removing anyOf attribute from {message_name} to avoid pjs error.')
