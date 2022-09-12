dc2c85ab4130b7d116ec9c9425e3f3099bf958ce
Alex Handler Wagner <Alex.Wagner@nationwidechildrens.org>
2022-09-12 10:33:59 -0400

    update tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,11 +5,10 @@ import yaml
 from schema.helpers import pjs_filter
 from ga4gh.gks.metaschema.tools.source_proc import YamlSchemaProcessor
 
-from config import vrs_json_path, vrs_yaml_path
+from config import vrs_json_path, vrs_yaml_path, vrs_merged_yaml_path
 
 # Are the yaml and json parsable and do they match?
-y = yaml.load(open(vrs_yaml_path), Loader=yaml.SafeLoader)
-p = YamlSchemaProcessor(y)
+p = YamlSchemaProcessor(vrs_yaml_path)
 j = json.load(open(vrs_json_path))
 
 
@@ -19,5 +18,5 @@ def test_json_yaml_match():
 
 # Can pjs handle this schema?
 def test_pjs_smoke():
-    ob = pjs.ObjectBuilder(pjs_filter(y))
+    ob = pjs.ObjectBuilder(pjs_filter(j))
     assert ob.build_classes()              # no exception => okay
