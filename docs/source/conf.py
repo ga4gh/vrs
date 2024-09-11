3828e4ef6a39eba2114e7104c8f5bdb29b9a4810
Kori Kuzma <korikuzma@gmail.com>
2024-09-10 23:08:23 -0400

    make navigation entries expandable (#550)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -75,6 +75,8 @@ todo_emit_warnings = True
 html_theme = 'sphinx_rtd_theme'
 html_logo = 'images/GA-logo.png'
 
+html_theme_options = {'collapse_navigation': False}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
