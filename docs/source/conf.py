0693d3fcefcbb1ab7183569115756e1e2fe0c7f3
James Stevenson <james.sharpsteen@gmail.com>
2026-02-04 13:40:00 -0500

    docs: fix orphaned pages showing up in search results (#672)
    * docs: fix orphan pages showing up in search

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,7 +68,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["def"]
+exclude_patterns = ["def/**"]
 
 # TODO directive output
 todo_include_todos = True
